e52af16b8478c9162265f60118890d9002db9e99
Amy King <amy@amyrom.tech>
2025-06-16 06:34:27 +0100

    Initial new site

---
+++ utils.py
@@ -0,0 +1,13 @@
+def generate_header():
+    print("""i
+i   OO O o o o...      ______________________
+i  O     ____          |                    |
+i ][_n_i_| (   ooo___  |  Amy's Gophersite  |
+i(__________|_[______]_|____________________|
+i  0--0--0      0  0      0       0     0    
+i""")
+    
+def generate_footer():
+    print("""
+i ------------------
+i Proudly written without AI, (c) 2025, Amy King""")
